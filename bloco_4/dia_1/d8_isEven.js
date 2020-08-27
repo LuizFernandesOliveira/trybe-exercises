26d61d09faf29b34d717c43f2b4f516c40fca7ff
LuizFernandesOliveira <luiz.fernandes@academico.ifrn.edu.br>
2020-08-27 17:55:30 -0300

    terminei o exercicio 9

--- bloco_4/dia_1/d8_isEven.js
+++ bloco_4/dia_1/d8_isEven.js
@@ -2,5 +2,5 @@ const number1 = 1;
 const number2 = 2;
 const number3 = 3;
 
-if(number1 % 2 == 0 || number2 % 2 == 0 || number3 % 2 == 0) console.log(true)
-else console.log(false)
+if(number1 % 2 == 0 || number2 % 2 == 0 || number3 % 2 == 0) console.log(true);
+else console.log(false);
