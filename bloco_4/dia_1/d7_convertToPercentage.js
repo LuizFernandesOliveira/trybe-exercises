bedcf3e209f97db740dfa892ddc0de29e774040b
LuizFernandesOliveira <luiz.fernandes@academico.ifrn.edu.br>
2020-08-27 17:49:53 -0300

    terminei o exercicio 7

---
+++ bloco_4/dia_1/d7_convertToPercentage.js
@@ -0,0 +1,9 @@
+const percentage = 80;
+const text = 'Conceito: '
+
+if(percentage >= 90) console.log(text+'A');
+else if(percentage >= 80) console.log(text+'B');
+else if(percentage >= 70) console.log(text+'C');
+else if(percentage >= 60) console.log(text+'D');
+else if(percentage >= 50) console.log(text+'E');
+else if(percentage < 50) console.log(text+'F');
