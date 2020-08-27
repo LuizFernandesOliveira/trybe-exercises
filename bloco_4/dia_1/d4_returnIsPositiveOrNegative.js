bc28a13baa8d099b6ae3a96ed2951abaee079043
LuizFernandesOliveira <luiz.fernandes@academico.ifrn.edu.br>
2020-08-27 17:27:45 -0300

    terminei o exercicio 5

--- bloco_4/dia_1/d4_returnIsPositiveOrNegative.js
+++ bloco_4/dia_1/d4_returnIsPositiveOrNegative.js
@@ -2,4 +2,4 @@ const valorDaVariavel = 0;
 
 if(valorDaVariavel > 0) console.log('positive');
 else if(valorDaVariavel < 0) console.log('negative');
-else console.log('zero')
+else console.log('zero');
